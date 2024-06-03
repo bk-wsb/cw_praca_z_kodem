3c3c2149471982fe69e8862b60fec9fceeff1b0e
bk-wsb <wrx81911@student.wroclaw.merito.pl>
2024-06-03 19:28:16 +0200

    Prosta aplikacja Flask

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask
+
+app = Flask(__name__)
+app.config['TESTING'] = True
+
+
+@app.route('/')
+@app.route('/index')
+def greeting():
+    return 'Hello, World!'
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
