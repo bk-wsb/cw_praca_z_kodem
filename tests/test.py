6bca042d9a1012403ff85cfbc1f37e11c5ee618a
bk-wsb <wrx81911@student.wroclaw.merito.pl>
2024-06-03 18:05:57 +0200

    Update test.py

--- tests/test.py
+++ tests/test.py
@@ -1,6 +1,7 @@
 from app import app
 
 def test_flask_greeding():
-    response = app.test_client().get('/')
-    assert response.status_code == 200
-    assert response.data == b'Hello, World!'
+    with app.test_client() as client:
+        response = app.test_client().get('/')
+        assert response.status_code == 200
+        assert response.data == b'Hello, World!'
